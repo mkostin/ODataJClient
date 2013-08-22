d036e312a94420d72d999a45c804139a28d8d286
fmartelli <fabio.martelli@gmail.com>
2013-08-22 12:22:09 +0200

    fixed auth url

--- engine/src/test/java/com/msopentech/odatajclient/engine/it/AbstractTest.java
+++ engine/src/test/java/com/msopentech/odatajclient/engine/it/AbstractTest.java
@@ -126,8 +126,7 @@ public abstract class AbstractTest {
         testOpenTypeServiceRootURL = testBaseURL + "/OpenTypeService.svc";
         testPrimitiveKeysServiceRootURL = testBaseURL + "/PrimitiveKeys.svc";
         testLargeModelServiceRootURL = testBaseURL + "/LargeModelService.svc";
-//        testAuthServiceRootURL = testDefaultServiceRootURL.replaceAll("8080", "9080");
-        testAuthServiceRootURL = "http://localhost:9080/DefaultService.svc";
+        testAuthServiceRootURL = testDefaultServiceRootURL.replaceAll("8080", "9080");
     }
 
     /**
