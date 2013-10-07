5c32040bdc217648c1ef38f0ca664dd27c02a9c0
Francesco Chicchiriccò <ilgrosso@apache.org>
2013-10-07 09:29:41 +0200

    Adding NTLM authentication feature #141

--- engine/src/main/java/com/msopentech/odatajclient/engine/client/http/AbstractNTLMAuthHttpClientFactory.java
+++ engine/src/main/java/com/msopentech/odatajclient/engine/client/http/AbstractNTLMAuthHttpClientFactory.java
@@ -21,27 +21,41 @@ package com.msopentech.odatajclient.engine.client.http;
 
 import java.net.URI;
 import org.apache.http.auth.AuthScope;
-import org.apache.http.auth.UsernamePasswordCredentials;
+import org.apache.http.auth.NTCredentials;
+import org.apache.http.client.CredentialsProvider;
 import org.apache.http.client.HttpClient;
+import org.apache.http.impl.client.BasicCredentialsProvider;
 import org.apache.http.impl.client.DefaultHttpClient;
 
 /**
- * Base implementation for working with Basic Authentication: needs to be subclassed in order to provide actual username
- * and password.
+ * Base implementation for working with NTLM Authentication via embedded HttpClient features: needs to be subclassed
+ * in order to provide all needed login information.
+ * <br/>
+ * External NTLM engine such as <a href="http://jcifs.samba.org/">JCIFS</a> library developed by the
+ * <a href="http://www.samba.org/">Samba</a> project as a part of their Windows interoperability suite of programs.
+ *
+ * @see NTCredentials
+ * @see http://hc.apache.org/httpcomponents-client-ga/ntlm.html#Using_Samba_JCIFS_as_an_alternative_NTLM_engine
  */
-public abstract class AbstractBasicAuthHttpClientFactory extends DefaultHttpClientFactory {
+public abstract class AbstractNTLMAuthHttpClientFactory extends DefaultHttpClientFactory {
 
     protected abstract String getUsername();
 
     protected abstract String getPassword();
 
+    protected abstract String getWorkstation();
+
+    protected abstract String getDomain();
+
     @Override
     public HttpClient createHttpClient(final HttpMethod method, final URI uri) {
         final DefaultHttpClient httpclient = (DefaultHttpClient) super.createHttpClient(method, uri);
 
-        httpclient.getCredentialsProvider().setCredentials(
-                new AuthScope(uri.getHost(), uri.getPort()),
-                new UsernamePasswordCredentials(getUsername(), getPassword()));
+        final CredentialsProvider credsProvider = new BasicCredentialsProvider();
+        credsProvider.setCredentials(AuthScope.ANY,
+                new NTCredentials(getUsername(), getPassword(), getWorkstation(), getDomain()));
+
+        httpclient.setCredentialsProvider(credsProvider);
 
         return httpclient;
     }
